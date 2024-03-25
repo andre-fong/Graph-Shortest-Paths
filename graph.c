4f853a1265855c2d17bd2d02ea587c91e870cdd7
Andre Fong <fongandre123@gmail.com>
2024-03-25 13:39:31 -0400

    feat: Graph and graph algos functions

--- graph.c
+++ graph.c
@@ -53,3 +53,119 @@ void printGraph(Graph* graph) {
 /*********************************************************************
  ** Required functions
  *********************************************************************/
+
+/* Returns a newly created Edge from vertex with ID 'fromVertex' to vertex
+ * with ID 'toVertex', with weight 'weight'.
+ */
+Edge* newEdge(int fromVertex, int toVertex, int weight) {
+	Edge *new = malloc(sizeof(Edge));
+	
+	// Check malloc successful
+	if (new == NULL) {
+		fprintf(stderr, "Could not allocate memory for new edge\n");
+		exit(1);
+	}
+	new->fromVertex = fromVertex;
+	new->toVertex = toVertex;
+	new->weight = weight;
+	
+	return new;
+}
+
+/* Returns a newly created EdgeList containing 'edge' and pointing to the next
+ * EdgeList node 'next'.
+ */
+EdgeList* newEdgeList(Edge* edge, EdgeList* next) {
+	EdgeList *newList = malloc(sizeof(EdgeList));
+	
+	// Check malloc successful
+	if (newList == NULL) {
+		fprintf(stderr, "Could not allocate memory for new edge list\n");
+		exit(1);
+	}
+	newList->edge = edge;
+	newList->next = next;
+	
+	return newList;
+}
+
+/* Returns a newly created Vertex with ID 'id', value 'value', and adjacency
+ * list 'adjList'.
+ * Precondition: 'id' is valid for this vertex
+ */
+Vertex* newVertex(int id, void* value, EdgeList* adjList) {
+	Vertex *new = malloc(sizeof(Vertex));
+	
+	// Check malloc successful
+	if (new == NULL) {
+		fprintf(stderr, "Could not allocate memory for new vertex\n");
+		exit(1);
+	}
+	new->id = id;
+	new->value = value;
+	new->adjList = adjList;
+	
+	return new;
+}
+
+/* Returns a newly created Graph with space for 'numVertices' vertices.
+ * Precondition: numVertices >= 0
+ */
+Graph* newGraph(int numVertices) {
+	Graph *new = malloc(sizeof(Graph));
+	
+	// Check malloc successful
+	if (new == NULL) {
+		fprintf(stderr, "Could not allocate memory for new graph\n");
+		exit(1);
+	}
+	new->numVertices = numVertices;
+	new->numEdges = 0;
+	
+	Vertex **vertices;
+	// Necessary since calloc(0) may return NULL on some implementations
+	if (numVertices == 0) {
+		vertices = NULL;
+	}
+	else {
+		vertices = (Vertex **)calloc(numVertices, sizeof(Vertex *));
+		if (vertices == NULL) {
+			fprintf(stderr, "Could not allocate memory for graph vertices list\n");
+			exit(1);
+		}
+	}
+	
+	new->vertices = vertices;
+	
+	return new;
+}
+
+/* Frees memory allocated for EdgeList starting at 'head'.
+ */
+void deleteEdgeList(EdgeList* head) {
+	EdgeList *pre = head;
+	EdgeList *tr = NULL;
+	while (pre != NULL) {
+		tr = pre->next;
+		free(pre->edge);	// free edge (edge lists have unique edges, ie. (a, b, 2) in a's list =/= (b, a, 2) in b's list)
+		free(pre);			// then free edge list node
+		pre = tr;
+	}
+}
+
+/* Frees memory allocated for 'vertex' including its adjacency list.
+ */
+void deleteVertex(Vertex* vertex) {
+	if (vertex == NULL) return;
+	deleteEdgeList(vertex->adjList);
+	free(vertex);
+}
+
+/* Frees memory allocated for 'graph'.
+ */
+void deleteGraph(Graph* graph) {
+	for (int i = 0; i < graph->numVertices; i++) {
+		deleteVertex(graph->vertices[i]);
+	}
+	free(graph);
+}
--- graph_algos.c
+++ graph_algos.c
@@ -12,6 +12,8 @@
 #define NOTHING -1
 #define DEBUG 0
 
+// TODO: Try running algo w/ graph of ZERO vertices (0)
+
 typedef struct records {
   int numVertices;    // total number of vertices in the graph
                       // vertex IDs are 0, 1, ..., numVertices-1
@@ -26,40 +28,200 @@ typedef struct records {
 /*************************************************************************
  ** Suggested helper functions -- part of starter code
  *************************************************************************/
-
-/* Creates, populates, and returns all records needed to run Prim's and
- * Dijkstra's algorithms on Graph 'graph' starting from vertex with ID
- * 'startVertex'.
- * Precondition: 'startVertex' is valid in 'graph'
- */
-Records* initRecords(Graph* graph, int startVertex);
+ 
+/* Returns true iff 'heap' is NULL or is empty. */
+bool isEmpty(MinHeap* heap) {
+	return (heap == NULL || heap->size == 0);
+}
 
 /* Creates, populates, and returns a MinHeap to be used by Prim's and
  * Dijkstra's algorithms on Graph 'graph' starting from vertex with ID
  * 'startVertex'.
  * Precondition: 'startVertex' is valid in 'graph'
  */
-MinHeap* initHeap(Graph* graph, int startVertex);
+MinHeap* initHeap(Graph* graph, int startVertex) {
+	MinHeap *new = newHeap(graph->numVertices);
+	
+	for (int i = 0; i < new->capacity; i++) {
+		insert(new, INT_MAX, i);
+	}
+	
+	decreasePriority(new, startVertex, 0);
+	
+	return new;
+}
 
-/* Returns true iff 'heap' is NULL or is empty. */
-bool isEmpty(MinHeap* heap);
-
-/* Prints the status of all current algorithm data: good for debugging. */
-void printRecords(Records* records);
+/* Creates, populates, and returns all records needed to run Prim's and
+ * Dijkstra's algorithms on Graph 'graph' starting from vertex with ID
+ * 'startVertex'.
+ * Precondition: 'startVertex' is valid in 'graph'
+ */
+Records* initRecords(Graph* graph, int startVertex) {
+	Records *newRecords = malloc(sizeof(Records));
+	
+	// Check malloc was successful
+	if (newRecords == NULL) {
+		fprintf(stderr, "Could not allocate memory for new record\n");
+		exit(1);
+	}
+	newRecords->numVertices = graph->numVertices;
+	
+	MinHeap *heap = initHeap(graph, startVertex);
+	newRecords->heap = heap;
+	
+	bool *finished = malloc(newRecords->numVertices * sizeof(bool));
+	for (int i = 0; i < newRecords->numVertices; i++) {
+		finished[i] = false;
+	}
+	newRecords->finished = finished;
+	
+	int *pred = malloc(newRecords->numVertices * sizeof(int));
+	for (int i = 0; i < newRecords->numVertices; i++) {
+		pred[i] = NOTHING;
+	}
+	pred[startVertex] = startVertex;	// set predecessor of start ind. to itself
+	newRecords->predecessors = pred;
+	
+	Edge *tree = malloc(newRecords->numVertices * sizeof(Edge));
+	newRecords->tree = tree;
+	
+	newRecords->numTreeEdges = 0;
+	
+	return newRecords;
+}
 
 /* Add a new edge to records at index ind. */
 void addTreeEdge(Records* records, int ind, int fromVertex, int toVertex,
-                 int weight);
+                 int weight) {
+	Edge edge;
+	edge.fromVertex = fromVertex;
+	edge.toVertex = toVertex;
+	edge.weight = weight;
+	records->tree[ind] = edge;
+	records->numTreeEdges++;	// to indicate we've added an edge to the tree
+}
 
 /* Creates and returns a path from 'vertex' to 'startVertex' from edges
  * in the distance tree 'distTree'.
  */
-EdgeList* makePath(Edge* distTree, int vertex, int startVertex);
+EdgeList* makePath(Edge* distTree, int vertex, int startVertex) {
+	// TODO
+	return NULL;
+}
+
+/* Frees all memory previously allocated for records. */
+void deleteRecords(Records *records) {
+	// TODO
+	return;
+}
+
+/* Get other vertex from edge in v.id's adjacency list */
+int getIdFromEdge(Edge *edge, int id) {
+	return (edge->fromVertex == id) ? edge->toVertex : edge->fromVertex;
+}
 
 /*************************************************************************
  ** Required functions
  *************************************************************************/
 
+/* Runs Prim's algorithm on Graph 'graph' starting from vertex with ID
+ * 'startVertex', and return the resulting MST: an array of Edges.
+ * Returns NULL if 'startVertex' is not valid in 'graph'.
+ * Precondition: 'graph' is connected.
+ */
+Edge* getMSTprim(Graph* graph, int startVertex) {
+	// Check startVertex is valid in graph
+	if (startVertex >= graph->numVertices || startVertex < 0) return NULL;
+	
+	// Initiate records for Prim's
+	Records *records = initRecords(graph, startVertex);
+	
+	while (!isEmpty(records->heap)) {
+		// Extract min and add to T
+		HeapNode min = extractMin(records->heap);
+		
+		records->finished[min.id] = true;
+		// Don't write (startVertex, startVertex, 0) to Edge array
+		if (min.id != startVertex) {
+			addTreeEdge(records, records->numTreeEdges, min.id, records->predecessors[min.id], min.priority);
+		}
+		
+		// Iterate through min's adjacency list
+		Vertex *v = graph->vertices[min.id];
+		EdgeList *tr = v->adjList;
+		
+		while (tr != NULL) {
+			int weight = tr->edge->weight;
+			int nextVertexId = getIdFromEdge(tr->edge, min.id);
+			
+			// If next vertex in heap and current weight < priority of next vertex, decrease priority of next vertex
+			if (!records->finished[nextVertexId] && weight < getPriority(records->heap, nextVertexId)) {
+				decreasePriority(records->heap, nextVertexId, weight);
+				records->predecessors[nextVertexId] = min.id;
+			}
+			
+			tr = tr->next;
+		}
+	}
+	
+	// TODO: Free mem
+	return records->tree;
+}
+
+/* Runs Dijkstra's algorithm on Graph 'graph' starting from vertex with ID
+ * 'startVertex', and return the resulting distance tree: an array of edges.
+ * Returns NULL if 'startVertex' is not valid in 'graph'.
+ * Precondition: 'graph' is connected.
+ */
+Edge* getDistanceTreeDijkstra(Graph* graph, int startVertex) {
+	// Check startVertex is valid in graph
+	if (startVertex >= graph->numVertices || startVertex < 0) return NULL;
+	
+	// Initiate records for Dijkstra's
+	Records *records = initRecords(graph, startVertex);
+	
+	while (!isEmpty(records->heap)) {
+		// Extract min and add to T
+		HeapNode min = extractMin(records->heap);
+		records->finished[min.id] = true;
+		// Write (u, u.pred) in index u for simplicity in later algo.
+		addTreeEdge(records, min.id, min.id, records->predecessors[min.id], min.priority);
+		
+		// Iterate through min's adjacency list
+		Vertex *v = graph->vertices[min.id];
+		EdgeList *tr = v->adjList;
+		
+		while (tr != NULL) {
+			int distance = min.priority + tr->edge->weight;
+			int nextVertexId = getIdFromEdge(tr->edge, min.id);
+			
+			// If next vertex in heap and current dist < priority of next vertex, decrease priority of next vertex
+			if (!records->finished[nextVertexId] && distance < getPriority(records->heap, nextVertexId)) {
+				decreasePriority(records->heap, nextVertexId, distance);
+				records->predecessors[nextVertexId] = min.id;
+			}
+			
+			tr = tr->next;
+		}
+	}
+	
+	// TODO: Free mem
+	return records->tree;
+}
+
+/* Creates and returns an array 'paths' of shortest paths from every vertex
+ * in the graph to vertex 'startVertex', based on the information in the
+ * distance tree 'distTree' produced by Dijkstra's algorithm on a graph with
+ * 'numVertices' vertices and with the start vertex 'startVertex'.  paths[id]
+ * is the list of edges of the form
+ *   [(id -- id_1, w_0), (id_1 -- id_2, w_1), ..., (id_n -- start, w_n)]
+ *   where w_0 + w_1 + ... + w_n = distance(id)
+ * Returns NULL if 'startVertex' is not valid in 'distTree'.
+ */
+EdgeList** getShortestPaths(Edge* distTree, int numVertices, int startVertex) {
+	
+	return NULL;
+}
 
 /*************************************************************************
  ** Provided helper functions -- part of starter code to help you debug!
